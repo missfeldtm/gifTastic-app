15c3daba6ca56282962092559758a36f0a5f3cc8
Mckenna <missfeldtm@gmail.com>
2019-05-10 21:48:49 -0500

    semi-final hw commit, currently making js more object oriented

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ hw-6
 5/7/19 - initial commit with basic html and assets folders
 
 5/8/19 - achieved the general search giphy function
+
+5/9/19 - figured out linking ajax request to different buttons
+
+5/10/19 - finished function project with play/pause animation
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,22 +8,81 @@ http://api.giphy.com/v1/gifs/search?&api_key=j80jTkTqUy0IVw0FRC2y0lp3NLCAqOCC&q=
 
 */
 
-var $form = $('form'),
-    $search = $(".search"),
-    $clear = $(".clear"),
-    $giphy = $(".giphy img"),
-    $giphyLink = $(".giphy a");
+//set all fxns into an object
+
+var library = {
+    generateButtons : function(){
+        for (var i = 0; i < animals.length; i++) {
+
+            createBtn(animals[i]);
+        }
+    },
+    
+}
+
+var gifDB = [];
+var stillDB = [];
+
+var imageStill;
+var gif;
+
+var animals = ['Golden Retriever', 'Newfoundland', 'Bernese Mountain Dog', 'Bassett Hound'];
 
 
+function generateButtons() {
+    for (var i = 0; i < animals.length; i++) {
 
-$('#addGiphy').on("click", function () {
+        createBtn(animals[i]);
+    }
+}
+
+function createBtn(val) {
+
+    var btn = $('<button>');
+
+    btn.attr({
+        dataName: "animals",
+        class: "btn dog",
+    });
+
+    btn.text(val);
+    $('#btn-area').append(btn);
+
+}
+
+$(document).on("click",".dog", function (){
+   
+    var search = $(this).text();
+    getGIFs(search);
+
+
+});
+
+
+function addButton() {
+    var dogVal = $('#giphy-input').val().trim();
+
+    //add value to array 
+    animals.push(dogVal);
+
+    //regenerate Buttons
+    createBtn(dogVal);
+
+    return false;
+}
+//
+function getGIFs(input){
+    gifDB = [];
+    stillDB = [];
 
     var url = "http://api.giphy.com/v1/gifs/search?q=";
     var apiKey = "&api_key=j80jTkTqUy0IVw0FRC2y0lp3NLCAqOCC";
 
+
+
     event.preventDefault();
 
-    var query = $('#giphy-input').val().trim();
+    var query = input;
 
 
     var queryURL = url + query + apiKey;
@@ -38,18 +97,32 @@ $('#addGiphy').on("click", function () {
         var results = response.data;
 
 
+
         console.log(results);
 
-        for (var i = 0; i < results.length; i++) {
+        for (var i = 0; i < 10; i++) {
 
             var animalDiv = $('<div>');
             var rating = results[i].rating;
+
+            imageStill = results[i].images.fixed_height_still.url;
+            gif = results[i].images.fixed_height.url;
+           
+            gifDB.push(gif);
+            stillDB.push(imageStill);
+
 
             console.log(results[i].rating);
             var p = $('<p>').text("Rating: " + rating);
 
             var gifIMG = $('<img>');
-            gifIMG.attr('src', results[i].images.fixed_height.url);
+
+            gifIMG.attr({
+                src: imageStill,
+                alt: 'stillframe of image',
+                class: 'still-img',
+                dataIndex: i
+            });
 
             animalDiv.append(p);
             animalDiv.append(gifIMG);
@@ -61,7 +134,38 @@ $('#addGiphy').on("click", function () {
 
 
 
-
     });
 
+};
+
+$('#addGiphy').on("click", function () {
+    getGIFs($('#giphy-input').val().trim());
+
+    addButton();
+    
 });
+
+
+$(document).on("click", ".still-img", function() {
+
+    var urlIndex = $(this).attr("dataIndex");
+    console.log(urlIndex);
+
+   if($(this).attr('src')===stillDB[urlIndex]){
+
+        $(this).attr("src", gifDB[urlIndex]);
+
+    }else{
+
+        $(this).attr("src", stillDB[urlIndex]);
+    }
+
+});
+
+$(document).on("click", "#clear", function() {
+
+$('.giphy').empty();
+
+});
+
+generateButtons();
